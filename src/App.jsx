e3ea3cb19f182cdf493d89a8dd15b2fa4e403160
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-11-04 09:34:03 +0330

    feature based driven folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
 }
 
 export default App;
