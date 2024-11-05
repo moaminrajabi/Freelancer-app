97ce991650abff65892198849a6216317e258a64
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-11-05 12:38:53 +0330

    add home  page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/completeProfile";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 function App() {
   const queryClient = new QueryClient();
@@ -15,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/complete-profile" element={<CompleteProfile />} />
-
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
